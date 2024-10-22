83b01032e953817006799748e857d00ffed55edc
student <student@BTU.local>
2024-10-22 10:42:01 +0400

    activities

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var bookName: EditText
+    lateinit var bookPrice: EditText
+    lateinit var bookPageNumber: EditText
+    lateinit var nextBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        bookName = findViewById(R.id.bookNameEditText)
+        bookPrice = findViewById(R.id.bookPriceEditText)
+        bookPageNumber = findViewById(R.id.pageNumberEditText)
+        nextBtn = findViewById(R.id.nextBtn)
+        nextBtn.setOnClickListener {
+            var name = bookName.text.toString()
+            var price = bookPrice.text.toString().toDouble()
+            var number = bookPageNumber.text.toString().toInt()
+        }
+
+
+
+    }
+}
