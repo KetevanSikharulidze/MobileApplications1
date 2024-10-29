135fddefd5e991c3cbe9c5a9e523588bc2a513fd
student <student@BTU.local>
2024-10-29 10:34:32 +0400

    activitebi davasrulet + Glide shemovitanet

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -20,10 +21,17 @@ class HomeActivity : AppCompatActivity() {
         bookPrice = findViewById(R.id.bookPriceEditText)
         bookPageNumber = findViewById(R.id.pageNumberEditText)
         nextBtn = findViewById(R.id.nextBtn)
+
         nextBtn.setOnClickListener {
             var name = bookName.text.toString()
             var price = bookPrice.text.toString().toDouble()
             var number = bookPageNumber.text.toString().toInt()
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("NAME",name)
+            intent.putExtra("PRICE",price)
+            intent.putExtra("NUMBER",number)
+            startActivity(intent)
+            finish()
         }
 
 
--- app/src/main/java/com/example/myapplication/SearchActivity.kt
+++ app/src/main/java/com/example/myapplication/SearchActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            bookNameTextview.text = intent.extras?.getString("NAME", "no name")
+            bookPageTextView.text = intent.extras?.getInt("NUMBER",0).toString()
+            bookPriceTextView.text = intent.extras?.getDouble("PRICE", 0.0).toString()
+            bookInStoreTextView.text = intent.extras?.getBoolean("INSTORE", false).toString()
+
+            val imageURL = intent.extras?.getString("URL","https://img.freepik.com/premium-psd/book-hardcover-mockup-three-views_125540-226.jpg")
+            Glide.with(this@SearchActivity).load(imageURL).into(bookImageView)
+
+        }
     }
 }
